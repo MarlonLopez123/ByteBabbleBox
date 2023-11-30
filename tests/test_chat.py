28101ca3af26532b3895cdcaf386006abb55ba7b
Angela Verdugo <angela.dev@ottis.com.co>
2023-11-29 19:56:03 -0500

    Modif test_chat

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -9,6 +9,10 @@ class TestChatServer(unittest.TestCase):
     def setUp(self):
         self.server_pid = self.get_server_pid()
 
+        # Espera hasta que el servidor esté listo (hasta que se imprima el mensaje [*] Server listening...)
+        while not self.is_server_ready():
+            time.sleep(1)
+
     def tearDown(self):
         if self.server_pid:
             os.kill(self.server_pid, signal.SIGTERM)
@@ -21,33 +25,33 @@ class TestChatServer(unittest.TestCase):
         except FileNotFoundError:
             return None
 
+    def is_server_ready(self):
+        with open('server_log.txt', 'r') as file:
+            log_content = file.read()
+            return "[*] Server listening on 0.0.0.0:12345" in log_content
+
     def test_client_server_interaction(self):
-        if self.server_pid:
-            # Server is running, don't start it again
-            return
-
-        # Start the server
-        server_thread = threading.Thread(target=self.run_server)
-        server_thread.start()
-        time.sleep(1)  # Wait for the server to start
-
-        # Your test code here
+        # Tu código de prueba aquí
+        # Puedes usar el código que ya tenías aquí
+        pass
 
     def run_server(self):
-        # Your server code here
+        # Tu código del servidor aquí
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind(('0.0.0.0', 12345))
         server.listen(5)
         print("[*] Server listening on 0.0.0.0:12345")
 
-        while True:
-            client, addr = server.accept()
-            print(f"[*] Accepted connection from: {addr[0]}:{addr[1]}")
-            client_handler = threading.Thread(target=self.handle_client, args=(client,))
-            client_handler.start()
+        # Guarda el log del servidor en 'server_log.txt'
+        with open('server_log.txt', 'w') as log_file:
+            while True:
+                client, addr = server.accept()
+                print(f"[*] Accepted connection from: {addr[0]}:{addr[1]}")
+                client_handler = threading.Thread(target=self.handle_client, args=(client,))
+                client_handler.start()
 
     def handle_client(self, client_socket):
-        # Your client handling code here
+        # Tu código de manejo del cliente aquí
         while True:
             data = client_socket.recv(1024)
             if not data:
